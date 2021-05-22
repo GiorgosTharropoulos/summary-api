4769c7daba95e861445dc325bac918b9a84e1c02
Giorgos Tharropoulos <Giorgos Tharropoulos>
2021-05-22 09:56:07 +0300

    Initial commit

---
+++ project/app/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI, Depends
+
+from app.config import get_settings, Settings
+
+app = FastAPI()
+
+
+@app.get('/ping')
+def pong(settings: Settings = Depends(get_settings)):
+    return {
+        'ping': 'pong',
+        'environment': settings.environment,
+        'testing': settings.testing
+    }
