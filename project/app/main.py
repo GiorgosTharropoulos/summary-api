f5f7897e8f033e1f9cd0e9c17988125fc0a79a9a
Giorgos Tharropoulos <Giorgos Tharropoulos>
2021-05-22 20:31:14 +0300

    Generate schema script in db.py.

--- project/app/main.py
+++ project/app/main.py
@@ -1,26 +1,32 @@
-import os
+import logging
 
 from fastapi import FastAPI, Depends
 from tortoise.contrib.fastapi import register_tortoise
 
 from app.config import Settings, get_settings
 from app.api import ping
+from app.db import init_db
+
+
+log = logging.getLogger("uvicorn")
 
 
 def create_application() -> FastAPI:
     application = FastAPI()
-
-    register_tortoise(
-        application,
-        db_url=os.environ.get("DATABASE_URL"),
-        modules={"models": ["app.models.tortoise"]},
-        generate_schemas=True,
-        add_exception_handlers=True,
-    )
-
     application.include_router(ping.router)
 
     return application
 
 
 app = create_application()
+
+
+@app.on_event("startup")
+async def startup_event():
+    log.info("Starting up.")
+    init_db(app)
+
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    log.info("Shuting down.")
