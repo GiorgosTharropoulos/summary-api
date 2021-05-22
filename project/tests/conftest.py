57ce0717ed513717e8bfd9e221c8388b47cba80a
Giorgos Tharropoulos <Giorgos Tharropoulos>
2021-05-22 20:19:07 +0300

    Test fixture uses the application factory

--- project/tests/conftest.py
+++ project/tests/conftest.py
@@ -1,11 +1,9 @@
 import os
 
 import pytest
+from app.main import create_application
+from app.config import Settings, get_settings
 from starlette.testclient import TestClient
-import requests
-
-from app import main
-from app.config import get_settings, Settings
 
 
 def get_settings_override():
@@ -14,7 +12,7 @@ def get_settings_override():
 
 @pytest.fixture(scope="module")
 def test_app():
-    main.app.dependency_overrides[get_settings] = get_settings_override
-    with TestClient(main.app) as test_client:
-
+    app = create_application()
+    app.dependency_overrides[get_settings] = get_settings_override
+    with TestClient(app) as test_client:
         yield test_client
