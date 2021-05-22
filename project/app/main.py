21cd5b951e9f74615da1fb71c87a0396c9919864
Giorgos Tharropoulos <Giorgos Tharropoulos>
2021-05-22 20:06:54 +0300

    Included ping router

--- project/app/main.py
+++ project/app/main.py
@@ -4,23 +4,21 @@ from fastapi import FastAPI
 from fastapi.params import Depends
 from tortoise.contrib.fastapi import register_tortoise
 
-from app.config import Settings, get_settings
-
-app = FastAPI()
-
-register_tortoise(
-    app,
-    db_url=os.environ.get("DATABASE_URL"),
-    modules={"models": ["app.models.tortoise"]},
-    generate_schemas=True,
-    add_exception_handlers=True,
-)
+from app.api import ping
 
 
-@app.get("/ping")
-async def pong(settings: Settings = Depends(get_settings)):
-    return {
-        "ping": "pong!",
-        "environment": settings.environment,
-        "testing": settings.testing,
-    }
+def create_application() -> FastAPI:
+    application = FastAPI()
+    register_tortoise(
+        application,
+        db_url=os.environ.get("DATABASE_URL"),
+        modules={"models": ["app.models.tortoise"]},
+        generate_schemas=True,
+        add_exception_handlers=True,
+    )
+    application.include_router(ping.router)
+
+    return application
+
+
+app = create_application()
