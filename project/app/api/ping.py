72255d0411c0307ea09d68cf1cafafb9ea30ab23
Giorgos Tharropoulos <Giorgos Tharropoulos>
2021-05-22 20:16:26 +0300

    Moved ping to router. Implemented app factory

---
+++ project/app/api/ping.py
@@ -0,0 +1,16 @@
+from re import M
+from typing import Set
+from fastapi import APIRouter, Depends
+
+from app.config import Settings, get_settings
+
+router = APIRouter()
+
+
+@router.get("/ping")
+async def pong(settings: Settings = Depends(get_settings)):
+    return {
+        "ping": "pong!",
+        "environment": settings.environment,
+        "testing": settings.testing,
+    }
